46a2de338b316533403e5634558af3a1b84554a8
EC2 Default User <ec2-user@ip-172-31-31-64.ap-northeast-1.compute.internal>
2021-04-18 13:42:13 +0900

    改善対応6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :edit_basic_all, :edit_over_work_day_approval, :update_over_work_day_approval]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :edit_basic_all]
-  before_action :correct_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:show, :edit]
   before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :update_basic_info, 
                 :edit_basic_all, :index_attendance]
   before_action :set_one_month, only: :show
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
-      redirect_to @user, order_sort: 1
+      redirect_to users_url
     else
       @users = User.paginate(page: params[:page], per_page: 20)
       render :edit
